5e91dc8385921b28722b52e2d79694f892a5ce1e
Oscar-Tark <oscartark91@gmail.com>
2020-11-21 15:29:29 +0100

    Initial Commit

---
+++ Scorpion_MongoDB_Library/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Scorpion_MongoDB_Library
+{
+    public class Mongodb
+    {
+        private static URL;
+
+
+        public Mongodb(string URL, int PORT)
+        {
+        }
+    }
+}
